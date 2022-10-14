48c74c524440ffc0ae1d1c971a19418f7727b9a2
Raiz <41806230+raizrazer@users.noreply.github.com>
2022-10-15 00:33:36 +0530

    Added Archive and other functionality

--- pages/todos/[slug].js
+++ pages/todos/[slug].js
@@ -1,7 +1,8 @@
-import Layout from '../../components/Layout'
-import { useRouter } from 'next/router'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { TodoCheckListItem } from "../../components/Todos/TodoCheckListItem";
 
 // * TODO ITEM CHECKLIST
 
@@ -9,66 +10,112 @@ export default function TodoItem() {
   const router = useRouter();
   const { slug } = router.query;
   const iconSize = 20;
-  const [todoItem,setTodoItem] = useState('');
+  // * Main Todo Item Title and Other Meta Data
+  const [todoItem, setTodoItem] = useState();
+  // * todoInputValue from the checklist Input
+  const [todoInputValue, setTodoInputValue] = useState("");
 
-  useEffect(()=>{
-    const getTodoItemData = async () =>{
-      const todoList = await JSON.parse(localStorage.getItem('todosMain'));
+  useEffect(() => {
+    const getTodoItemData = async () => {
+      const todoList = await JSON.parse(localStorage.getItem("todosMain"));
       const todoItem = todoList[slug];
       setTodoItem(todoItem);
-    }
+    };
     getTodoItemData();
-  },[slug])
+  }, [slug]);
 
+  //* HANDLES THE SUMBIT of CREATE A TODO
+  const handleClick = (e) => {
+    if (e.key === "Enter") {
+      if (todoInputValue !== "") {
+        setTodoInputValue("");
+        const date = new Date();
+        // ? MAIN VALUE of the input creation
+        const todoInputValue = {
+          content: todoInputValue,
+          day: date.getDate(),
+          month: date.getMonth() + 1,
+          year: date.getFullYear(),
+          hour: date.getHours(),
+          minute: date.getMinutes(),
+          second: date.getSeconds(),
+          data: [],
+        };
+        const todoList = JSON.parse(localStorage.getItem("todosMain"));
+        const gotoIndex = todoList.length;
+        todoList.push(todoInputValue);
+        localStorage.setItem("todosMain", JSON.stringify(todoList));
+        router.push(`/todos/${gotoIndex}`);
+      }
+    }
+  };
   return (
     <Layout>
+      {todoItem && (
         <div className="todoCheckListMain">
-            <div className="todoMainEnter">
-              <div className="todoMainEnter_Top">
-                <div className="title">
-                  <p>{(todoItem) ? `${todoItem.title}  [${todoItem.hour>12 ? todoItem.hour%12 : todoItem.hour}:${todoItem.minute<10 ? "0"+((todoItem.minute).toString()) : todoItem.minute} ${(todoItem.hour>12)? `PM` : `AM`}] [${todoItem.day}-${todoItem.month}-${todoItem.year}]` : `Loading` }</p>
-                </div>
-                <div className="rightside">
-                  <div className="buttons">
-                    <Image width={iconSize} height={iconSize} src="/svg/archive.svg" alt="archive" />
-                    <Image width={iconSize} height={iconSize} src="/svg/delete-alt.svg" alt="delete" />
-                  </div>
-                  <div className="percentage">
-                    <p>5%</p>
-                  </div>
-                </div>
+          <div className="todoMainEnter">
+            <div className="todoMainEnter_Top">
+              <div className="title">
+                <p>
+                  {todoItem
+                    ? `${todoItem.title}  [${
+                        todoItem.hour > 12 ? todoItem.hour % 12 : todoItem.hour
+                      }:${
+                        todoItem.minute < 10
+                          ? "0" + todoItem.minute.toString()
+                          : todoItem.minute
+                      } ${todoItem.hour > 12 ? `PM` : `AM`}] [${todoItem.day}-${
+                        todoItem.month
+                      }-${todoItem.year}]`
+                    : `Loading`}
+                </p>
               </div>
-              <div className="todoMainEnter_Bottom">
-                <div className="taskInput">
-                  <input
-                    type="text"
-                    name="createtodo"
-                    id="createTask"
-                    placeholder="Type the task..."
+              <div className="rightside">
+                <div className="buttons">
+                  <Image
+                    width={iconSize}
+                    height={iconSize}
+                    src="/svg/archive.svg"
+                    alt="archive"
                   />
-                  <Image width={iconSize} height={iconSize} src="/svg/ui-add.svg" alt="add" />
+                  <Image
+                    width={iconSize}
+                    height={iconSize}
+                    src="/svg/delete-alt.svg"
+                    alt="delete"
+                  />
                 </div>
-  
-                <div className="taskItem" draggable="true">
-                  <div className="leftside">
-                    <div className="dragger">
-                      <Image width={iconSize} height={iconSize} src="/svg/dragger.svg" alt="dragme" />
-                    </div>
-                    <div className="task-content">
-                      <p>Code the developer</p>
-                    </div>
-                  </div>
-                  <div className="rightside">
-                    <div className="buttons">
-                      <Image width={iconSize} height={iconSize} src="/svg/edit.svg" alt="edit" />
-                      <Image width={iconSize} height={iconSize} src="/svg/delete-alt.svg" alt="remove" />
-                    </div>
-                    <div className="time"><p>11:53 PM</p></div>
-                  </div>
+                <div className="percentage">
+                  <p>5%</p>
                 </div>
               </div>
             </div>
+            <div className="todoMainEnter_Bottom">
+              <div className="taskInput">
+                <input
+                  type="text"
+                  name="createtodo"
+                  id="createTask"
+                  placeholder="Type the task..."
+                  value={todoInputValue}
+                  onChange={(e) => {
+                    setTodoInputValue(e.target.value);
+                  }}
+                  onKeyDown={() => handleClick()}
+                />
+                <Image
+                  width={iconSize}
+                  height={iconSize}
+                  src="/svg/ui-add.svg"
+                  alt="add"
+                />
+              </div>
+
+              <TodoCheckListItem content={"LMAO WHAT THE FUCK"} />
+            </div>
           </div>
+        </div>
+      )}
     </Layout>
-  )
+  );
 }
